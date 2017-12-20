9f7f557d4a38b2f8dff86e634c0611a6e84be72a
Tom Moir <trmoir9@gmail.com>
2017-12-20 12:19:27 +0000

    pre merge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import './index.css';
 
 class FilterDiv extends React.Component {
@@ -15,9 +17,11 @@ class FilterDiv extends React.Component {
 class LogoDiv extends React.Component {
   render () {
     return (
-      <div id="logo">
-        <h1>Holly</h1>
-      </div>
+      <Router>
+        <div id="logo">
+          <Link to="/"><h1>Holly</h1></Link>
+        </div>
+      </Router>
     );
   }
 }
@@ -25,15 +29,21 @@ class LogoDiv extends React.Component {
 class BasketDiv extends React.Component {
   render () {
     return (
-      <div id="basket" className="panel">
-        <img src='basket.png' alt="basket"/>
-      </div>
+      <Router>
+        <div id="basket" className="panel">
+          <Switch>
+          <Link to="/my-basket">
+            <img src='basket.png' alt="basket"/>
+          </Link>
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
 
-export default class HeaderBlock extends React.Component {
+class HeaderBlock extends React.Component {
   renderLogoDiv(){
     return <LogoDiv/>;
   }
@@ -58,24 +68,47 @@ export default class HeaderBlock extends React.Component {
 }
 
 class ProductList extends React.Component {
-  render(){
-    return <div id="product-list"></div>;
+  render () {
+    return (
+        <div id="product-list">
+          This is the Product List
+        </div>
+    );
+  }
+}
+
+class MyBasket extends React.Component {
+  render () {
+    return (
+      <div id="my-basket">
+        This is my basket!
+      </div>
+    );
   }
 }
 
 class HomePage extends React.Component {
-  renderHeaderBlock(){
+  renderHeaderBlock () {
     return <HeaderBlock />;
   }
-  renderProductList() {
-    return <ProductList />;
-  }
-  render() {
+  //
+  // renderProductList () {
+  //   return <ProductList />;
+  // }
+
+  render () {
     return (
-      <div id="container">
-      {this.renderHeaderBlock()}
-      {this.renderProductList()}
-      </div>
+        <div id="container">
+          {this.renderHeaderBlock()}
+        <Router>
+          <div id="display">
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route path="/my-basket" component={MyBasket} />
+            </Switch>
+          </div>
+        </Router>
+    </div>
     );
   }
 }
