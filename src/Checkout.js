5ff249901c2c1cd78be74c12f85c84c8ea25b1b1
Tom Moir <trmoir9@gmail.com>
2017-12-22 15:56:28 +0000

    fixed grammar on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -8,7 +8,7 @@ export class Checkout extends Component {
           <h1>Checkout</h1>
         </div>
         <div className="item-list">
-          <h3>Success! Your items are on there way!</h3>
+          <h3>Success! Your items are on their way!</h3>
         </div>
       </div>
     );
