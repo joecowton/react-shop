d008fd44ae6d4543e2ca5f74a9556a4d6ae09336
Tom Moir <trmoir9@gmail.com>
2017-12-22 15:30:54 +0000

    changing sizes of product pages on home page

--- src/Product.js
+++ src/Product.js
@@ -8,15 +8,15 @@ export class Product extends Component {
             <img className="thumb" src={this.props.image} alt={this.props.name} />
           </div>
           <div>
-            <div>{this.props.name}</div>
+            <div className="itemName">{this.props.name}</div>
             <div>
-              <div className="split">
-                £{parseFloat(this.props.price).toFixed(2)}
-              </div>
-              <div className="split">
-                <button className="add-to-basket" onClick={() => this.props.handleAddItem(this.props)}>
-                  <img src="add-to-cart-x1.png" alt="cart-icon" />
-                </button>
+                <div className="itemName">
+                  £{parseFloat(this.props.price).toFixed(2)}
+                </div>
+                <div>
+                  <button className="add-to-basket" onClick={() => this.props.handleAddItem(this.props)}>
+                    <img src="add-to-cart-x1.png" alt="cart-icon" />
+                  </button>
               </div>
             </div>
           </div>
